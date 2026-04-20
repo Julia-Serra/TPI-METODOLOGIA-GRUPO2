5bb8cf9bc720c72d52e9015803b8b5c7cc0df88f
nazareno11 <nazarenoluna86@gmail.com>
2026-04-20 09:11:12 -0300

    spring

---
+++ src/main/java/com/metodologia/bodyPaint/BodyPaintApplication.java
@@ -0,0 +1,13 @@
+package com.metodologia.bodyPaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyPaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyPaintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metodologia/bodyPaint/BodyPaintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metodologia.bodyPaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyPaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
