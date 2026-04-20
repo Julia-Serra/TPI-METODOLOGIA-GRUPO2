23ded35a11a25f2ad661de40a901a8b73d51b907
TobiasPagniez <tobiaspagniez1@gmail.com>
2026-04-20 11:11:26 -0300

    config

---
+++ src/main/java/com/metodologia/bodyPaint/config/exceptions/CustomException.java
@@ -0,0 +1,21 @@
+package com.metodologia.bodyPaint.config.exceptions;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public abstract class CustomException extends RuntimeException {
+    
+    private final HttpStatus status;
+    private final List<String> errors;
+
+    public CustomException(String message, HttpStatus status, List<String> errors) {
+        super(message);
+        this.status = HttpStatus.BAD_REQUEST;
+        this.errors = List.of(message);
+    }
+
+}
