5772dc76639746751985a49f65f0644c81cd080e
valentinolm <vlmvalenlozanomassey@gmail.com>
2026-04-27 09:37:36 -0300

    validaciones dtos

--- src/main/java/com/metodologia/bodyPaint/feature/dtos/request/ProductoRequest.java
+++ src/main/java/com/metodologia/bodyPaint/feature/dtos/request/ProductoRequest.java
@@ -3,22 +3,25 @@ package com.metodologia.bodyPaint.feature.dtos.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 @AllArgsConstructor
 @Data
 public class ProductoRequest {
 
-    @NotBlank
+    @NotBlank(message = "Por favor, ingrese un nombre")
+    @Size(min = 1, max = 50, message = "El nombre debe tener entre 1 y 50 caracteres")
     private String nombre;
 
-    @NotBlank
+    @NotBlank(message = "Por favor, ingrese una marca")
+    @Size(min = 1, max = 50, message = "La marca debe tener entre 1 y 50 caracteres")
     private String marca;
 
-    @Positive
+    @Positive(message = "El precio debe ser mayor a 0)")
     private double precio;
 
-    @PositiveOrZero
+    @PositiveOrZero(message = "El stock debe ser mayor o igual a 0")
     private int stock;
 
     private String imagen;
