100b1f18066480113959a6edebb2cc60473be26b
valentinolm <vlmvalenlozanomassey@gmail.com>
2026-05-16 15:17:08 -0300

    commit de la segunda historia de usuario de vandedor

---
+++ src/main/java/com/metodologia/bodyPaint/feature/services/impl/domain/ActualizarPedidoService.java
@@ -0,0 +1,26 @@
+package com.metodologia.bodyPaint.feature.services.impl.domain;
+
+import com.metodologia.bodyPaint.config.exceptions.BadRequestException;
+import com.metodologia.bodyPaint.feature.dtos.request.ActualizarEstadoPedidoRequest;
+import com.metodologia.bodyPaint.feature.models.Pedido;
+import com.metodologia.bodyPaint.feature.repositories.PedidoRepository;
+import com.metodologia.bodyPaint.feature.services.interfaces.domain.IActualizarPedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActualizarPedidoService implements IActualizarPedidoService {
+
+    private PedidoRepository pedidoRepository;
+
+    @Override
+    public void actualizarEstado(Long pedidoId, ActualizarEstadoPedidoRequest request) {
+        Pedido pedido = pedidoRepository.findById(pedidoId)
+                .orElseThrow(() -> new BadRequestException("Pedido no encontrado"));
+
+        pedido.actualizarEstado(request.getEstado());
+
+        pedidoRepository.save(pedido);
+    }
+}
