62484f5b81e614f61be1ff31ca6579f499dd5540
nazareno11 <nazarenoluna86@gmail.com>
2026-06-01 09:51:33 -0300

    asdsad

--- src/main/java/com/metodologia/bodyPaint/feature/dtos/request/ClienteRequest.java
+++ src/main/java/com/metodologia/bodyPaint/feature/dtos/request/ClienteRequest.java
@@ -2,6 +2,7 @@ package com.metodologia.bodyPaint.feature.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
