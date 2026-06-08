289379bf4ef57d95d78dc5c41ed412b733d0dd8c
Tobias Pagniez <tobiaspagniez1@gmail.com>
2026-06-08 01:56:28 -0300

    merge: fusionar main en tobias y actualizar imagenes de uploads

--- src/main/java/com/metodologia/bodyPaint/config/WebConfig.java
+++ src/main/java/com/metodologia/bodyPaint/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
-    }
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:./uploads/"); // El "./" le dice explícitamente "parate en la raíz del proyecto"
+}
 }
